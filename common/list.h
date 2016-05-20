b1a07259ec18cfef1003b16bcac4c01091ab6a3f
Amir Sharif <amirsharifzadeh@gmail.com>
2016-05-20 14:20:27 +0000

    Upload new file

---
+++ common/list.h
@@ -0,0 +1,14 @@
+//
+// Created by Amir on 5/19/2016.
+//
+#include "monitor.h"
+
+#ifndef DARTSYNC_UTILS_H
+#define DARTSYNC_UTILS_H
+
+void addFileToList(FileList *fileList, FileInfo *fileInfo);
+
+FileInfo *PopList(FileList *fileList);
+
+#endif //DARTSYNC_UTILS_H
+
